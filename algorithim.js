9ce754338879a0a131a8f2498af3cf92fc39a17a
sherif <sherif.elshamaaa@gmail.com>
2022-06-02 11:59:29 +0200

    update

--- algorithim.js
+++ algorithim.js
@@ -1,21 +1,21 @@
 var set1 = [12, 13, 6, 10];
 var set2 = [13, 10, 16, 15];
 
-function overlapSum(x,y) {
-	var sum = 0;
-	for(i in x){
-		if(y.includes(x[i])){
-			sum += x[i];
-		}
-	}
-	for(i in y){
-		if(x.includes(y[i])){
-			sum += y[i];
-		}
-	}
-	console.log(sum);
+function overlapSum(x, y) {
+  var sum = 0;
+  for (i in x) {
+    if (y.includes(x[i])) {
+      sum += x[i];
+    }
+  }
+  for (i in y) {
+    if (x.includes(y[i])) {
+      sum += y[i];
+    }
+  }
+  console.log(sum);
 }
-overlapSum(set1,set2);
+overlapSum(set1, set2);
 
 
 
@@ -41,9 +41,10 @@ function distinctSumHash(x, y) {
   var sum = 0;
   var z = x.concat(y);
   var hashTable = z.reduce(
-    (key, val) => ((key[val] = (key[val] || 0) + 1), key),
+    (acc, key) => ((acc[key] = (acc[key] || 0) + 1), acc),
     {}
   );
+
   console.log(hashTable);
   Object.keys(hashTable).map(function (key) {
     if (hashTable[key] == 1) {
@@ -53,5 +54,26 @@ function distinctSumHash(x, y) {
   console.log(sum);
 }
 distinctSumHash(set1, set2);
+
+function SumHash(x, y) {
+  var sum = 0;
+  var z = x.concat(y);
+  console.log(z)
+  var object = {};
+  for (var i in z) {
+    if (!object[z[i]]) {
+      object[z[i]] = 1;
+    } else {
+      ++object[z[i]]
+    }
+  }
+  Object.keys(object).forEach(function (key) {
+    if (object[key] == 1) {
+      sum += parseInt(key)
+    }
+  })
+  console.log(object, sum);
+}
+SumHash(set1, set2)
 
 
